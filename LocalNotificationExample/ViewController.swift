7c6eadd9cea14a553b8d3761a34b7759851794e1
Sean Goldsborough <smgoldsborough@gmail.com>
2018-01-06 21:01:43 -0500

    Initial Commit

---
+++ LocalNotificationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalNotificationExample
+//
+//  Created by Sean Goldsborough on 1/6/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
